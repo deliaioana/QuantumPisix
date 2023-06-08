8eebee821f7bd6fc4c956d743b1ed246f6668c8f
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2023-06-08 23:32:32 +0300

    Work restart

--- levels.py
+++ levels.py
@@ -5,15 +5,15 @@ class Levels:
     def __init__(self):
         self.levels = []
 
-        level_1 = level.Level([('foxy', 'asleep')], [], 1)
-        level_2 = level.Level([('miso', 'idle'), ('miso', 'idle')], ['kiss_gate', 'catnip_gate'], 1)
+        level_1 = level.Level([('foxy', 'asleep')], ['catnip_gate'], 1)
+        level_2 = level.Level([('miso', 'idle'), ('miso', 'idle')], ['catnip_gate', 'catnip_gate'], 1)
         level_3 = level.Level([('foxy', 'idle'), ('peanut', 'idle'), ('cookie', 'idle')], ['kiss_gate'], 1)
         level_4 = level.Level([('miso', 'asleep'), ('foxy', 'asleep'), ('peanut', 'asleep'), ('cookie', 'asleep')],
                               ['catnip_gate', 'catnip_gate'], 1)
-        level_5 = level.Level([('miso', 'idle')], ['kiss_gate'], 2)
-        level_6 = level.Level([('foxy', 'idle')], ['kiss_gate'], 3)
-        level_7 = level.Level([('cookie', 'idle')], ['kiss_gate'], 4)
-        level_8 = level.Level([('peanut', 'idle')], ['kiss_gate'], 5)
+        level_5 = level.Level([('miso', 'idle')], ['catnip_gate'], 2)
+        level_6 = level.Level([('foxy', 'idle')], ['catnip_gate'], 3)
+        level_7 = level.Level([('cookie', 'idle')], ['catnip_gate'], 4)
+        level_8 = level.Level([('peanut', 'idle')], ['catnip_gate'], 5)
 
         self.levels.append(0)
         self.levels.append(level_1)
