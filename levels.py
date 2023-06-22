305040e2381c43e03318e900c5b52aa6656805ae
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2023-06-22 04:12:06 +0300

    Superposition animation added

--- levels.py
+++ levels.py
@@ -5,8 +5,8 @@ class Levels:
     def __init__(self):
         self.levels = []
 
-        level_1 = level.Level([('foxy', 'asleep')], ['milk_gate', 'catnip_gate', 'box_gate', 'cat-food_gate',
-                                                     'mouse_gate'], 1, "", [1])
+        level_1 = level.Level([('cookie', 'super'), ('peanut', 'super'), ('foxy', 'super'), ('miso', 'super')],
+                              ['milk_gate', 'catnip_gate', 'box_gate', 'cat-food_gate', 'mouse_gate'], 1, "", [1])
         level_2 = level.Level([('miso', 'idle'), ('miso', 'idle')], ['milk_gate'], 1, "", [1, 1])
         level_3 = level.Level([('foxy', 'idle'), ('peanut', 'idle'), ('miso', 'asleep'), ('cookie', 'idle')],
                               ['catnip_gate'], 1, "", [1, 1, 1])
@@ -15,7 +15,7 @@ class Levels:
                               5, "", [0, 0, 0, 0])
         level_5 = level.Level([('miso', 'idle')], ['catnip_gate'], 2, "", [1])
         level_6 = level.Level([('foxy', 'idle')], ['catnip_gate'], 3, "", [1])
-        level_7 = level.Level([('cookie', 'idle')], ['catnip_gate'], 4, "", [1])
+        level_7 = level.Level([('cookie', 'super'), ('peanut', 'super')], ['catnip_gate'], 4, "", [1])
         level_8 = level.Level([('peanut', 'idle')], ['catnip_gate'], 5, "", [2])
 
         self.levels.append(0)
